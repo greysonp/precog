73d4551e79b7f459a2527af05e17037979fc7409
Greyson Parrelli <greyson.parrelli@gmail.com>
2014-03-28 13:07:25 -0400

    Movement.

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -1,12 +1,78 @@
 (function() {
     var scene = new cutie.Scene();
 
+    var player = {};
+    var moveType = {
+        'UP': 0,
+        'RIGHT': 1,
+        'DOWN': 2,
+        'LEFT': 3,
+        'NONE': 4
+    }
+    var moving = {};
+
      scene.init = function(preloaded) {
-        // Declare pairs of button texts and scene names
-        var titleText = new createjs.Text('precog', '36px Arial', '#000000');
-        titleText.x = cutie.WIDTH/2 - titleText.getMeasuredWidth()/2;
-        titleText.y = 40;
-        return titleText;
+        // Init player
+        player = new cutie.Shape();
+        player.graphics.beginFill('#000000').drawRect(0, 0, 10, 10);
+        player.width = 10;
+        player.height = 10;
+        player.speed = .5;
+        player.x = cutie.WIDTH/2 - player.width/2;
+        player.y = cutie.HEIGHT/2 - player.height/2;
+        this.addChild(player);
+
+        // Init game timer
+        setInterval(spawnEnemy, 100);
+
+        // Determine movement
+        cutie.getStage().on('stagemousedown', updateMovement);
+        cutie.getStage().on('stagemousedown', function() {
+            moving = moveType.NONE;
+        });
+        cutie.getStage().on('stagemousemove', updateMovement);
+    }
+
+    scene.tick = function() {
+        switch(moving) {
+            case moveType.RIGHT:
+                player.x +=  player.speed;
+                break;
+            case moveType.LEFT:
+                player.x -=  player.speed;
+                break;
+            case moveType.UP:
+                player.y -=  player.speed;
+                break;
+            case moveType.DOWN:
+                player.y +=  player.speed;
+                break;
+        }
+    }
+
+    function updateMovement() {
+        var stage = cutie.getStage();
+        var midX = cutie.WIDTH/2;
+        var midY = cutie.HEIGHT/2;
+        var topY = cutie.HEIGHT/3;
+        var bottomY = cutie.HEIGHT/3 * 2;
+        if (stage.mouseX  > midX && stage.mouseY > topY && stage.mouseY < bottomY) {
+            moving = moveType.RIGHT;
+        }
+        else if (stage.mouseX < midX && stage.mouseY > topY && stage.mouseY < bottomY) {
+            moving = moveType.LEFT;
+        }
+        else if (stage.mouseY < midY) {
+            moving = moveType.UP;
+        }
+        else {
+            moving = moveType.DOWN;
+        }
+    }
+
+
+    function spawnEnemy() {
+        console.log('spawn');
     }
 
     cutie.registerScene(scene, 'game');
--- js/scenes/title.js
+++ js/scenes/title.js
@@ -2,17 +2,23 @@
     var scene = new cutie.Scene();
 
      scene.init = function(preloaded) {
-        // Declare pairs of button texts and scene names
-        var titleText = new createjs.Text('precog.', '125px Arial', '#000000');
+        // Title text
+        var titleText = new cutie.Text('precog.', '125px Arial', '#000000');
         titleText.x = cutie.WIDTH/2 - titleText.getMeasuredWidth()/2;
         titleText.y = 40;
         this.addChild(titleText);
 
-        var playText = new createjs.Text('play.', '100px Arial', '#000000');
+        // Play button
+        var playBtn = new cutie.Shape();
+        playBtn.graphics.beginFill('#ffffff').drawRect(0, 0, 300, 200);
+        playBtn.x = cutie.WIDTH/2 - 150;
+        playBtn.y = 700 - 150;
+        this.addChild(playBtn);
+        var playText = new cutie.Text('play.', '100px Arial', '#000000');
         playText.x = cutie.WIDTH/2 - playText.getMeasuredWidth()/2;
         playText.y = 600;
         this.addChild(playText);
-        playText.addEventListener('click', function() {
+        playBtn.addEventListener('click', function() {
             cutie.setScene('game');
         });
     }
